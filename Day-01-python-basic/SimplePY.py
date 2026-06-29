8f3601d1072206c64d52ab626c39cb9db4462d22
vignesh-dev7 <vigneshzdev@gmail.com>
2026-06-29 17:11:14 +0000

    Initial commit

---
+++ Day-01-python-basic/SimplePY.py
@@ -0,0 +1,10 @@
+marks = int(input("Please Enter your age: "))
+
+if marks >= 90:
+    print("Grade S")
+elif marks >=70:
+    print("Grade A")
+elif marks >=50:
+    print("Grade C")
+else:
+    print("Fail")
